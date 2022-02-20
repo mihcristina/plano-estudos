e2ca5a8006bfa227227c8699d7329531fb0692c8
mihcristina <contato@michellicristina.com.br>
2022-02-19 23:00:10 -0300

    chore: add reminder functions

---
+++ PlanoDeEstudos/StudyPlan.swift
@@ -0,0 +1,25 @@
+//
+//  StudyPlain.swift
+//  PlanoDeEstudos
+//
+//  Created by Michelli Cristina de Paulo Lima on 15/02/22.
+//  Copyright © 2022 Eric Brito. All rights reserved.
+//
+
+import Foundation
+
+class StudyPlan: Codable {
+    let course: String
+    let section: String
+    let date: Date
+    var done: Bool = false
+    var id: String
+    
+    init(course: String, section: String, date: Date, done: Bool, id: String) {
+        self.course = course
+        self.section = section
+        self.date = date
+        self.done = done
+        self.id = id
+    }
+}
