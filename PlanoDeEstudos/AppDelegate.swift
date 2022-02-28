32873121e906ffd0eea30a4dc4f998fe004fc79a
mihcristina <contato@michellicristina.com.br>
2022-02-27 23:19:11 -0300

    chore: configure appDelegate

--- PlanoDeEstudos/AppDelegate.swift
+++ PlanoDeEstudos/AppDelegate.swift
@@ -13,10 +13,36 @@ import UserNotifications
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let center = UNUserNotificationCenter.current()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         window?.tintColor = UIColor(named: "main")
+        
+        center.delegate = self
+        center.getNotificationSettings { (settings) in
+            if settings.authorizationStatus == .notDetermined {
+                let options: UNAuthorizationOptions = [.alert, .sound, .badge, .carPlay]
+                self.center.requestAuthorization(options: options) { success, error in
+                    if error == nil {
+                        print(success)
+                    } else {
+                        print(error!.localizedDescription)
+                    }
+                }
+            } else if settings.authorizationStatus == .denied {
+                print("Usuário negou a Notificação")
+            }
+        }
+        
+        let confirmAction = UNNotificationAction(identifier: "Confirm", title: "Já estudei", options: [.foreground])
+        let cancelAction = UNNotificationAction(identifier: "Cancel", title: "Cancelar", options: [])
+        if #available(iOS 12.0, *) {
+            let category = UNNotificationCategory(identifier: "Lembrete", actions: [confirmAction, cancelAction], intentIdentifiers: [], hiddenPreviewsBodyPlaceholder: "", categorySummaryFormat: "", options: [.customDismissAction])
+            center.setNotificationCategories([category])
+        } else {
+            // Fallback on earlier versions
+        }
         return true
     }
 
@@ -42,4 +68,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        let id = response.notification.request.identifier
+        print(id)
+        
+        switch response.actionIdentifier {
+        case "Confirm":
+            print("Usuário confirmou")
+        case "Cancel":
+            print("Usuário cancelou")
+        default:
+            break
+        }
+        completionHandler()
+    }
 }
